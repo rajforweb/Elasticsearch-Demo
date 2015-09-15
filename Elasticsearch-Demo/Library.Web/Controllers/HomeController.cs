318cae76de9fabbe067608f2b1cb4ab0f7971ac0
rajforweb <rajkumar12345@gmail.com>
2015-09-14 23:28:28 -0400

    Sample app
    This app includes elastic search demo using Nest.

---
+++ Elasticsearch-Demo/Library.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Web.Models;
+using Library.Web.ViewModels;
+
+namespace Library.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SearchService _searchService;
+        public HomeController()
+        {
+            _searchService = new SearchService();
+        }
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Search(string query, int page = 0, int pageSize = 10)
+        {
+
+            var result = _searchService.Find(query, page, pageSize);
+            var suggestion = _searchService.FindPhraseSuggestion(query, 0, 3);
+
+            var viewModel = new SearchViewModel { Query = query, Results = result.Item1,Elapsed = result.Item2, Suggestions = suggestion };
+
+
+            return View("Index", viewModel);
+        }
+
+    }
+}
---
+++ Elasticsearch-Demo/Library.Web/Elasticsearch/SearchService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Mvc;
+using Library.Web.Models;
+using Nest;
+
+namespace Library.Web.Controllers
+{
+    public class SearchService
+    {
+        public double MinScore { get {return 0.0005; }}
+        public string PreHighlightTag
+        {
+            get { return @"<strong>"; }
+        }
+
+        public string PostHighlightTag
+        {
+            get { return @"</strong>"; }
+        }
+
+
+        public Tuple< IEnumerable<IHit<Book>>,long> Find(string query, int page = 0, int pageSize = 10)
+        {
+            var result = Elasticsearch.Elasticsearch.Client.Search<Book>(s => s
+                 .From(page * pageSize)
+                 .Size(pageSize)
+                 .MinScore(MinScore)
+                 .Highlight(h => h
+                     .PreTags(PreHighlightTag)
+                     .PostTags(PostHighlightTag)
+                     .OnFields(
+                         f => f.OnField(b => b.Description),
+                         f => f.OnField(b => b.Title)
+                         ))
+                 .Query(q => q.QueryString(qs => qs.Query(query).UseDisMax())));
+
+            return new Tuple<IEnumerable<IHit<Book>>, long>(result.Hits,result.ElapsedMilliseconds); 
+        }
+
+        public IDictionary<string, Suggest[]> FindPhraseSuggestion(string phrase, int page = 0, int pageSize = 5)
+        {
+            var result = Elasticsearch.Elasticsearch.Client.Search<Book>(s => s
+                .From(page*pageSize)
+                .Size(pageSize)
+                .SuggestPhrase("did-you-mean", ps => ps
+                    .Text(phrase)
+                    .OnField(f => f.Description))
+                .Query(q => q.MatchAll()));
+            
+            return result.Suggest;
+        }
+
+
+        public IEnumerable<IHit<Book>> FindAll()
+        {
+            var result = Elasticsearch.Elasticsearch.Client.Search<Book>(s => s.AllIndices());
+            return result.Hits;
+        }
+
+    }
+}
